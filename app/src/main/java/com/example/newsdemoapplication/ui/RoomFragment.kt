5351a77b124ab975944a050af35e6f8d3d7e373b
guzhipeng <1281747107@qq>
2021-01-20 16:17:37 +0800

    点击标点跳转新页面

--- app/src/main/java/com/example/newsdemoapplication/ui/RoomFragment.kt
+++ app/src/main/java/com/example/newsdemoapplication/ui/RoomFragment.kt
@@ -10,11 +10,13 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
+import androidx.navigation.fragment.NavHostFragment
 import com.example.newsdemoapplication.R
 import com.example.newsdemoapplication.dsl.*
 import com.example.newsdemoapplication.main.MainActivity
 import com.example.newsdemoapplication.main.MainActivity1
 import com.example.newsdemoapplication.room.Word
+import com.example.newsdemoapplication.widget.PageTitleBar
 import com.lishuaihua.toast.ToastUtils
 import com.lxj.xpopup.XPopup
 
@@ -30,95 +32,70 @@ class RoomFragment :Fragment(){
     private val queryId by lazy {
         MutableLiveData(0L)
     }
-    private val firstColumn by lazy {
-        LinearLayout{
-            layout_height= match_parent
-            layout_width= wrap_content
-            orientation= vertical
-            bgShape(10, R.color.blue)
-            TextView { weight=1f
-                text= "get"
-                padding=10
-                bgShape(6,R.color.light_green)}.addOnClick {
-                ToastUtils.show("getwordDao")
-                Log.e("TAG", "${allWord.value?.size}.:${allWord.value.toString()} ", )
-            }
-            TextView { weight=1f
-                text= "insert"
-                padding=10
-                bgShape(6,R.color.light_green)}.addOnClick {
-                ToastUtils.show("addwordDao")
-                mainViewModel.insert(Word(count++,"$$count",name.also { name+=name }))
-            }
-            TextView { weight=1f
-                text= "insert"
-                padding=10
-                bgShape(6,R.color.light_green)}.addOnClick {
-                ToastUtils.show("getwordDao")
-//                (requireActivity().application as NewsApp).wordDb.getWordDao()
-            }
-        }
-    }
-    private val secondColumn by lazy {
-
-    }
     private val contentView by lazy {
         LinearLayout {
             padding=10
             layout_height= match_parent
             layout_width= match_parent
-            LinearLayout{
-                layout_height= match_parent
-                layout_width= wrap_content
-                orientation= vertical
-                bgShape(10, R.color.blue)
-                TextView { weight=1f
-                    text= "get"
-                    padding=10
-                    bgShape(6,R.color.light_green)}.addOnClick {
-                    ToastUtils.show("getwordDao")
-                    Log.e("TAG", "${allWord.value?.size}.:${allWord.value.toString()} ", )
-                }
-                TextView { weight=1f
-                    text= "insert"
-                    padding=10
-                    bgShape(6,R.color.light_green)}.addOnClick {
-                    ToastUtils.show("addwordDao")
-                    mainViewModel.insert(Word(count++,"$$count",name.also { name+=name }))
-                }
-                TextView { weight=1f
-                    text= "insert"
-                    padding=10
-                    bgShape(6,R.color.light_green)}.addOnClick {
-                    ToastUtils.show("getwordDao")
+            orientation= vertical
+            PageTitleBar(context).also {
+                it.tvTile.text="点击标题后进入的新页面，后续按需求补充"
+                it.btnBack.addOnClick { NavHostFragment.findNavController(this@RoomFragment).popBackStack() }
+                addView(it) }
+            LinearLayout {
+                LinearLayout{
+                    layout_height= match_parent
+                    layout_width= wrap_content
+                    orientation= vertical
+                    bgShape(10, R.color.blue)
+                    TextView { weight=1f
+                        text= "get"
+                        padding=10
+                        bgShape(6,R.color.light_green)}.addOnClick {
+                        ToastUtils.show("getwordDao")
+                        Log.e("TAG", "${allWord.value?.size}.:${allWord.value.toString()} ", )
+                    }
+                    TextView { weight=1f
+                        text= "insert"
+                        padding=10
+                        bgShape(6,R.color.light_green)}.addOnClick {
+                        ToastUtils.show("addwordDao")
+                        mainViewModel.insert(Word(count++,"$$count",name.also { name+=name }))
+                    }
+                    TextView { weight=1f
+                        text= "insert"
+                        padding=10
+                        bgShape(6,R.color.light_green)}.addOnClick {
+                        ToastUtils.show("getwordDao")
 //                (requireActivity().application as NewsApp).wordDb.getWordDao()
-                }
-            }
-            LinearLayout{
-                layout_height= match_parent
-                layout_width= wrap_content
-                orientation= vertical
-                bgShape(10, R.color.blue)
-
-                TextView { weight=1f
-                    observe(queryId){
-                        text=it.toString()
                     }
-                }.addOnClick {
-                    XPopup.Builder(context).asInputConfirm("输入Id",""){
-                        it.toLongOrNull()?.let {
-                            queryId.value=it
+                }
+                LinearLayout{
+                    layout_height= match_parent
+                    layout_width= wrap_content
+                    orientation= vertical
+                    bgShape(10, R.color.blue)
+
+                    TextView { weight=1f
+                        observe(queryId){
+                            text=it.toString()
                         }
-                    }.show()
-                }
-                TextView{ weight=1f
-                    text="queryById"
-                }.addOnClick {
-                    observe(mainViewModel.queryById(queryId.value!!)){
-                        Log.e("TAG", ": $it", )
-                        it.toast
+                    }.addOnClick {
+                        XPopup.Builder(context).asInputConfirm("输入Id",""){
+                            it.toLongOrNull()?.let {
+                                queryId.value=it
+                            }
+                        }.show()
                     }
+                    TextView{ weight=1f
+                        text="queryById"
+                    }.addOnClick {
+                        observe(mainViewModel.queryById(queryId.value!!)){
+                            Log.e("TAG", ": $it", )
+                            it.toast
+                        }
 
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/example/newsdemoapplication/widget/PageTitleBar.kt
@@ -0,0 +1,36 @@
+package com.example.newsdemoapplication.widget
+
+import android.content.Context
+import android.service.quicksettings.Tile
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.newsdemoapplication.R
+import com.example.newsdemoapplication.dsl.*
+import java.util.jar.Attributes
+
+class PageTitleBar(context: Context,attributes: AttributeSet?=null):FrameLayout(context,attributes) {
+    lateinit var btnBack:ImageView
+    lateinit var btnRight:LinearLayout
+    lateinit var tvTile: TextView
+    val contentView by lazy {
+        LinearLayout(match_parent,44){
+            gravity= gravity_center_vertical
+            padding=3
+            btnBack=ImageView(24,24){
+                src= R.drawable.ic_arrow_back_black_24dp
+            }
+            tvTile =TextView(){
+                weight=1f
+                text="标题"
+                textStyle= bold
+            }
+            btnRight=LinearLayout(24,24)
+        }
+    }
+    init {
+        contentView
+    }
+}
